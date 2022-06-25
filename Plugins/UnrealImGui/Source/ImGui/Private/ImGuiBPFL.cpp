668f6b1ab673a3ed3f3d09fd4fabccd4d84ae56b
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2022-06-26 01:26:52 +0200

    ImGui - ImGuiBPFL - UEnum ImGui_WindowFlags
    - dodałem enum, które można odczytać w blueprintach
    - dodałem switch, który który konwertuje pojedyncze ImGui_WindowFlags na prawidłowe ImGuiWindowFlags_ z <ImGui.h>
    - drobne poprawki

--- Plugins/UnrealImGui/Source/ImGui/Private/ImGuiBPFL.cpp
+++ Plugins/UnrealImGui/Source/ImGui/Private/ImGuiBPFL.cpp
@@ -7,17 +7,19 @@
 
 //Public
 
-void UImGuiBPFL::PrintSimpleWindow(FString Name, FString Text)
+void UImGuiBPFL::PrintSimpleWindow(FString Name, FString Text, FVector2D RelativeScreenPosition)
 {
 	std::string ConvertBuffer = TCHAR_TO_UTF8(*Name);
-	ImGui::Begin(&*ConvertBuffer.begin(), nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	UImGuiBPFL::SetNextWindowPosRelative(RelativeScreenPosition, ImGuiCond_Always);
 	ImGui::Begin(&*ConvertBuffer.begin(), nullptr,
 		//ImGuiWindowFlags_AlwaysAutoResize +
 		//ImGuiWindowFlags_NoBackground +
 		//ImGuiWindowFlags_NoCollapse +
-		ImGuiWindowFlags_NoSavedSettings +
 		//ImGuiWindowFlags_NoResize +
 		//ImGuiWindowFlags_NoInputs +
+		ImGuiWindowFlags_NoSavedSettings +
+		ImGuiWindowFlags_AlwaysAutoResize +
+		ImGuiWindowFlags_NoMove +
 		ImGuiWindowFlags_None
 	);
 	ConvertBuffer = TCHAR_TO_UTF8(*Text);
@@ -67,6 +69,8 @@ void UImGuiBPFL::PrintWatermarkFixed(FString Name, FString Text, FVector2D Relat
 	UImGuiBPFL::SetNextWindowPosRelative(RelativeScreenPosition, ImGuiCond_Always);
 	ImGui::SetNextWindowBgAlpha(BackgroundAlpha);
 
+	//ImGui::SetNextWindowFocus();	//TODO: always-on-top
+
 	std::string ConvertBuffer
 		= TCHAR_TO_UTF8(*Name);
 	ImGui::Begin(&*ConvertBuffer.begin(), nullptr, flags);
@@ -96,5 +100,88 @@ void UImGuiBPFL::TextMousePosition()
 		ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
 	else
 		ImGui::Text("Mouse Position: <invalid>");
+}
+
+ImGuiWindowFlags UImGuiBPFL::GetFixedWidnowFlag(ImGui_WindowFlags Flag)
+{
+	switch (Flag)
+	{
+	case NoTitleBar:
+		return ImGuiWindowFlags_NoTitleBar;
+		break;
+	case NoResize:
+		return ImGuiWindowFlags_NoResize;
+		break;
+	case NoMove:
+		return ImGuiWindowFlags_NoMove;
+		break;
+	case NoScrollbar:
+		return ImGuiWindowFlags_NoScrollbar;
+		break;
+	case NoScrollWithMouse:
+		return ImGuiWindowFlags_NoScrollWithMouse;
+		break;
+	case NoCollapse:
+		return ImGuiWindowFlags_NoCollapse;
+		break;
+	case AlwaysAutoResize:
+		return ImGuiWindowFlags_AlwaysAutoResize;
+		break;
+	case NoBackground:
+		return ImGuiWindowFlags_NoBackground;
+		break;
+	case NoSavedSettings:
+		return ImGuiWindowFlags_NoSavedSettings;
+		break;
+	case NoMouseInputs:
+		return ImGuiWindowFlags_NoMouseInputs;
+		break;
+	case MenuBar:
+		return ImGuiWindowFlags_MenuBar;
+		break;
+	case HorizontalScrollbar:
+		return ImGuiWindowFlags_HorizontalScrollbar;
+		break;
+	case NoFocusOnAppearing:
+		return ImGuiWindowFlags_NoFocusOnAppearing;
+		break;
+	case NoBringToFrontOnFocus:
+		return ImGuiWindowFlags_NoBringToFrontOnFocus;
+		break;
+	case AlwaysVerticalScrollbar:
+		return ImGuiWindowFlags_AlwaysVerticalScrollbar;
+		break;
+	case AlwaysHorizontalScrollbar:
+		return ImGuiWindowFlags_AlwaysHorizontalScrollbar;
+		break;
+	case AlwaysUseWindowPadding:
+		return ImGuiWindowFlags_AlwaysUseWindowPadding;
+		break;
+	case NoNavInputs:
+		return ImGuiWindowFlags_NoNavInputs;
+		break;
+	case NoNavFocus:
+		return ImGuiWindowFlags_NoNavFocus;
+		break;
+	case UnsavedDocument:
+		return ImGuiWindowFlags_UnsavedDocument;
+		break;
+	case NoNav:
+		return ImGuiWindowFlags_NoNav;
+		break;
+	case NoDecoration:
+		return ImGuiWindowFlags_NoDecoration;
+		break;
+	case NoInputs:
+		return ImGuiWindowFlags_NoInputs;
+		break;
+	case None:
+		return ImGuiWindowFlags_None;
+		break;
+	default:
+		UE_LOG(LogTemp, Warning, TEXT("'ImGuiWindowFlags UImGuiBPFL::GetFixedWidnowFlag(ImGui_WindowFlags Flag)' method just returned 'None' due to reching defailt case in switch on ImGui_WindowFlags. Probably new value was added to enum but not to switch."));
+		return ImGuiWindowFlags_None;
+		break;
+	}
 }
 
--- Plugins/UnrealImGui/Source/ImGui/Public/ImGuiBPFL.h
+++ Plugins/UnrealImGui/Source/ImGui/Public/ImGuiBPFL.h
@@ -4,10 +4,39 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include <ImGui.h>
 #include "ImGuiBPFL.generated.h"
 
-typedef int ImGuiCond; // <-- z jakiegoœ powodu Linker tego nie ³¹czy przy kompilacji, ogarn¹æ ocb - najprawdopodobniej potrzeba pliku nag³ówkowego
 
+
+UENUM(BlueprintType)//, Category = "DearImGui")
+enum ImGui_WindowFlags
+{
+	NoTitleBar                   UMETA(DisplayName = "NoTitleBar"),                  // Disable title-bar
+	NoResize                     UMETA(DisplayName = "NoResize"),                    // Disable user resizing with the lower-right grip
+	NoMove                       UMETA(DisplayName = "NoMove"),                      // Disable user moving the window
+	NoScrollbar                  UMETA(DisplayName = "NoScrollbar"),                 // Disable scrollbars (window can still scroll with mouse or programmatically)
+	NoScrollWithMouse            UMETA(DisplayName = "NoScrollWithMouse"),           // Disable user vertically scrolling with mouse wheel. On child window, mouse wheel will be forwarded to the parent unless NoScrollbar is also set.
+	NoCollapse                   UMETA(DisplayName = "NoCollapse"),                  // Disable user collapsing window by double-clicking on it. Also referred to as Window Menu Button (e.g. within a docking node).
+	AlwaysAutoResize             UMETA(DisplayName = "AlwaysAutoResize"),            // Resize every window to its content every frame
+	NoBackground                 UMETA(DisplayName = "NoBackground"),                // Disable drawing background color (WindowBg, etc.) and outside border. Similar as using SetNextWindowBgAlpha(0.0f).
+	NoSavedSettings              UMETA(DisplayName = "NoSavedSettings"),             // Never load/save settings in .ini file
+	NoMouseInputs                UMETA(DisplayName = "NoMouseInputs"),               // Disable catching mouse, hovering test with pass through.
+	MenuBar                      UMETA(DisplayName = "MenuBar"),                     // Has a menu-bar
+	HorizontalScrollbar          UMETA(DisplayName = "HorizontalScrollbar"),         // Allow horizontal scrollbar to appear (off by default). You may use SetNextWindowContentSize(ImVec2(width,0.0f)); prior to calling Begin() to specify width. Read code in imgui_demo in the "Horizontal Scrolling" section.
+	NoFocusOnAppearing           UMETA(DisplayName = "NoFocusOnAppearing"),          // Disable taking focus when transitioning from hidden to visible state
+	NoBringToFrontOnFocus        UMETA(DisplayName = "NoBringToFrontOnFocus"),       // Disable bringing window to front when taking focus (e.g. clicking on it or programmatically giving it focus)
+	AlwaysVerticalScrollbar      UMETA(DisplayName = "AlwaysVerticalScrollbar"),     // Always show vertical scrollbar (even if ContentSize.y < Size.y)
+	AlwaysHorizontalScrollbar    UMETA(DisplayName = "AlwaysHorizontalScrollbar"),   // Always show horizontal scrollbar (even if ContentSize.x < Size.x)
+	AlwaysUseWindowPadding       UMETA(DisplayName = "AlwaysUseWindowPadding"),      // Ensure child windows without border uses style.WindowPadding (ignored by default for non-bordered child windows, because more convenient)
+	NoNavInputs                  UMETA(DisplayName = "NoNavInputs"),                 // No gamepad/keyboard navigation within the window
+	NoNavFocus                   UMETA(DisplayName = "NoNavFocus"),                  // No focusing toward this window with gamepad/keyboard navigation (e.g. skipped by CTRL+TAB)
+	UnsavedDocument              UMETA(DisplayName = "UnsavedDocument"),             // Display a dot next to the title. When used in a tab/docking context, tab is selected when clicking the X + closure is not assumed (will wait for user to stop submitting the tab). Otherwise closure is assumed when pressing the X, so if you keep submitting the tab may reappear at end of tab bar.
+	NoNav                        UMETA(DisplayName = "NoNav"),
+	NoDecoration                 UMETA(DisplayName = "NoDecoration"),
+	NoInputs                     UMETA(DisplayName = "NoInputs"),
+	None                         UMETA(DisplayName = "Null")
+};
 
 UCLASS()
 class IMGUI_API UImGuiBPFL : public UBlueprintFunctionLibrary
@@ -17,7 +46,7 @@ class IMGUI_API UImGuiBPFL : public UBlueprintFunctionLibrary
 public:
 
 	UFUNCTION(BlueprintCallable, Category = "DearImGui", meta=(DevelopmentOnly))
-	static void PrintSimpleWindow(FString Name, FString Text);
+	static void PrintSimpleWindow(FString Name, FString Text, FVector2D RelativeScreenPosition);
 
 	UFUNCTION(BlueprintCallable, Category = "DearImGui")
 	static void PrintWatermark(FString Name, FString Text, FVector2D RelativeScreenPosition, bool bPrintTextOnly, float BackgroundAlpha, bool bDevelopmentOnly);
@@ -33,4 +62,6 @@ private:
 	static void SetNextWindowPosRelative(FVector2D RelativeScreenPosition, ImGuiCond Condition);
 
 	static void TextMousePosition();
+
+    static ImGuiWindowFlags GetFixedWidnowFlag(ImGui_WindowFlags Flag);
 };
